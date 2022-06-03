90d53f40c73df6b4d834992f6ef3230a8b2990d6
qasimalik@mailbox.unideb.hu <qasimalik@mailbox.unideb.hu>
2022-06-04 00:38:16 +0200

    format changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
 
 const HatsPage = () => (
   <div>
@@ -15,8 +15,8 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );
